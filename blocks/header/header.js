e4e80dd0c560f02e1964c02018b3d25bf2094e8c
vishabhguptaadobe <45848500+vishabhguptaadobe@users.noreply.github.com>
2026-09-14 11:29:53 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -119,12 +119,14 @@ export default async function decorate(block) {
     }
 
     // toc button for mobile toc view
-    const button = nav.querySelector('.nav-toc-btn')
-    const a_tag = button.querySelector('a')
-    a_tag.addEventListener('click', evt => {
-      evt.preventDefault()
-      toggleMenu(nav, navSections)
-    })
+    const button = nav.querySelector('.nav-toc-btn');
+const a_tag = button ? button.querySelector('a') : null;
+if (a_tag) {
+  a_tag.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    toggleMenu(nav, navSections);
+  });
+}
     
 
 
